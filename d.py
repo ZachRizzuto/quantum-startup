4423d39f465be8f906c1aa4eaf788c443f535f64
Zachary Rizzuto <133621132+ZachRizzuto@users.noreply.github.com>
2026-03-18 13:55:38 -0500

    Create radio buttons that filter data by region

--- d.py
+++ d.py
@@ -2,7 +2,7 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 
-from dash import Dash, html, dcc
+from dash import Dash, Input, Output, html, dcc
 import plotly.express as px
 import pandas as pd
 
@@ -24,6 +24,7 @@ fig = px.line(
     line_shape="linear"
     )
 
+
 app.layout = html.Div(children=[
     html.H1(children="Sales Data"),
     html.Div(children="""
@@ -31,9 +32,54 @@ app.layout = html.Div(children=[
     """),
     dcc.Graph(
         id="sales-graph",
-        figure=fig
-    )
+        figure=fig,
+    ),
+    html.Div(children=[
+        dcc.RadioItems(
+            id="region-filter",
+            options=[
+                {"label": "All Regions", "value": "all"},
+                {"label": "North", "value": "north"},
+                {"label": "South", "value": "south"},
+                {"label": "East", "value": "east"},
+                {"label": "West", "value": "west"},
+            ],
+            value="all",
+            labelStyle={"display": "flex", "margin-right": "10px", "font-size": "32px", "color": "#333"},
+            style={"display": "flex", "justify-content": "center", "margin-top": "20px"}
+        )
+    ], style={"margin-top": "20px", "display": "flex", "flex-direction": "row", "justify-content": "center"})
 ])
+
+@app.callback(
+    Output("sales-graph", "figure"),
+    Input("region-filter", "value")
+)
+
+def filter_data(region):
+    if region == "all":
+        return px.line(
+            df, x="date", 
+            y="total_sales", 
+            color="region", 
+            title="Product Sales Data", 
+            labels={"date": "Date", "total_sales": "Total Sales", "region": "Region"}, 
+            template="plotly_dark", 
+            markers=True, 
+            line_shape="linear"
+    )
+    else:
+        filtered_df = df[df["region"] == region]
+        return px.line(
+            filtered_df, x="date", 
+            y="total_sales", 
+            color="region", 
+            title="Product Sales Data", 
+            labels={"date": "Date", "total_sales": "Total Sales", "region": "Region"}, 
+            template="plotly_dark", 
+            markers=True, 
+            line_shape="linear"
+        )
 
 if __name__ == '__main__':
     app.run(debug=True)
